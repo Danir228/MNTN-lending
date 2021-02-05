7d06fdcce067917853ff31348268ea2e90c4ae26
Данир Низамов <bezymniy228@gmail.com>
2021-02-05 23:59:32 +0300

    Исправил баги в скроллах, добавил анимацию для слайдер меню

--- js/script.js
+++ js/script.js
@@ -1,64 +1,88 @@
     // Scroll down window.............................................................
+    function downWindow() {
+        let scrollheight = document.documentElement.scrollHeight;
+        let heightClient = document.documentElement.clientHeight;
+        let u;
 
-    function goBottom() {
-        var match = Math.ceil(window.pageYOffset + document.documentElement.clientHeight);
-
-        if (match != document.documentElement.scrollHeight) {
-            window.scrollBy(0, 15);
-            setTimeout(goBottom, 0);
+        function down() {
+            let scrollTop = document.documentElement.scrollTop;
+            if((scrollTop + heightClient) < (scrollheight - 1)) {
+                window.scrollBy (0, 100);
+                u = setTimeout(down, 10);
+            } else {
+                clearTimeout(u);
+            }
         }
+        down();
     }
 
-    const scrollDownWindow = document.querySelector(".section_window_scroll-item");
+    const scrollDownWindow = document.querySelector('.section_window_scroll-item');
 
-    scrollDownWindow.addEventListener('click', goBottom);
+    scrollDownWindow.addEventListener('click', downWindow);
 
     // Scroll up window.........................................................................
 
-    function backToTop() {
-        if (window.pageYOffset > 0) {
-            window.scrollBy(0, -15);
-            setTimeout(backToTop, 0);
+    let t;
+
+    function up() {
+        let top = Math.max(
+            document.body.scrollTop,
+            document.documentElement.scrollTop);
+        if(top > 0) {
+            window.scrollBy(0,((top+150)/-10));
+            t = setTimeout(up,20);
+        } else {
+            clearTimeout(t);
         }
     }
 
     const arrowTop = document.querySelector(".arrowTop");
 
-    arrowTop.addEventListener('click', backToTop);
+    arrowTop.addEventListener('click', up);
 
     window.addEventListener('scroll', function() {
-        arrowTop.hidden = (window.pageYOffset < document.documentElement.clientHeight);
+        arrowTop.hidden = (
+            window.pageYOffset < document.documentElement.clientHeight
+        );
     });
 
     // Scroll to the anchor........................................................................
 
-    const anchors = [].slice.call(document.querySelectorAll('.scroll_menu a[href*="#"]')),
-            animationTime = 400,
-            framesCount = 30;
+    const anchors = [].slice.call(
+        document.querySelectorAll('.scroll_menu a[href*="#"]')),
+        animationTime = 500,
+        framesCount = 20;
 
-    function hiddenElementParent() {
+    function animationElementParent() {
         anchors.forEach(item => {
             item.classList.remove('show-btn');
         });
     }
 
     anchors.forEach(function(item) {
-            item.addEventListener('click', function(e) {
-                const target = e.target;
-                e.preventDefault();
+        item.addEventListener('click', function(e) {
+            const target = e.target;
+            e.preventDefault();
 
-                hiddenElementParent();
+            animationElementParent();
 
-                if(target == item) {
-                    item.classList.add('show-btn');
-                }
+            if(target == item) {
+                item.classList.add('show-btn');
+            }
 
-            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
+            let coordY = document.querySelector(
+                item.getAttribute('href')).getBoundingClientRect().top +
+                window.pageYOffset;
 
             let scroller = setInterval(function() {
                 let scrollBy = coordY / framesCount;
 
-                if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                if (
+                    scrollBy > window.pageYOffset -
+                    coordY && window.innerHeight +
+                    window.pageYOffset <
+                    document.body.offsetHeight
+                ) {
                     window.scrollBy(0, scrollBy);
                 } else {
                     window.scrollTo(0, coordY);
@@ -85,12 +109,37 @@
         modalOverlay.classList.toggle("closed");
     });
 
+    // Animation for Slider-Menu..............................
 
+    const animate_slide = [].slice.call(
+        document.querySelectorAll('.animate_slide'));
 
+    const slider_menu = [].slice.call(
+        document.querySelectorAll('.section_slider-1'));
 
+    function addAnimate(index) {
+        slider_menu.forEach(function(item, i){
+            if(index == i) {
+                item.classList.add('show-btn');
+            }
+        });
+    }
 
+    function removeAnimate() {
+        slider_menu.forEach(function(item) {
+            item.classList.remove('show-btn');
+        });
+    }
 
-
+    animate_slide.forEach(function(item, i) {
+        let indexSlide = i;
+        item.addEventListener('mouseleave', () => {
+            removeAnimate();
+        });
+        item.addEventListener('mouseenter', () => {
+            addAnimate(indexSlide);
+        });
+    });
 
 
 
